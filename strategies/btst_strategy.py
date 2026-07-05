04129c2909a8f3a857af9ae96561f4d97fd15287
Sriram S <sriram.sridharv@gmail.com>
2026-07-05 19:51:23 +0530

    Add wavetrend_buy rule to BTST_RULES

--- strategies/btst_strategy.py
+++ strategies/btst_strategy.py
@@ -1,7 +1,8 @@
 BTST_RULES = {
     "ema20_above_ema50": True,
+    "close_above_ema20": True,
     "rsi_min": 55,
     "adx_min": 20,
     "volume_multiplier": 1.5,
-    "wavetrend": True,
+    "wavetrend_buy": True,
 }
