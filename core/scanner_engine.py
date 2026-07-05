09421fd0586bee13fa47e410c5bea31ebec50966
Sriram S <sriram.sridharv@gmail.com>
2026-07-05 19:55:15 +0530

    Add BTST strategy configuration
    Added configuration for BTST strategy with parameters.

--- core/scanner_engine.py
+++ core/scanner_engine.py
@@ -1,1 +1,13 @@
-scan_market(strategy="BTST")
+"""
+BTST Strategy Rules
+"""
+
+BTST_CONFIG = {
+    "ema_fast": 20,
+    "ema_slow": 50,
+    "rsi_min": 55,
+    "adx_min": 20,
+    "volume_multiplier": 1.5,
+    "close_above_ema20": True,
+    "wavetrend_buy": True,
+}
