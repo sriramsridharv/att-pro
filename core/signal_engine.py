57d6c317748a427770a7c5ac90dd56261399f529
Sriram S <sriram.sridharv@gmail.com>
2026-07-05 19:46:20 +0530

    Add signal data structure for trading decision

--- core/signal_engine.py
+++ core/signal_engine.py
@@ -1,1 +1,11 @@
-
+{
+    "signal": "BUY",
+    "score": 92,
+    "reasons": [
+        "EMA20 > EMA50",
+        "RSI 61",
+        "ADX 28",
+        "Volume 2.3x",
+        "WaveTrend Buy"
+    ]
+}
