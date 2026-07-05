3585196791f70c1cf069ce14f7935f5c7cdd5fd9
Sriram S <sriram.sridharv@gmail.com>
2026-07-05 20:43:58 +0530

    Implement test for IndicatorEngine with MarketData
    Added test for calculating indicators using MarketData.

--- tests/test_indicator_engine.py
+++ tests/test_indicator_engine.py
@@ -1,1 +1,12 @@
+from data.market_data import MarketData
+from core.indicator_engine import IndicatorEngine
 
+market = MarketData()
+
+indicator = IndicatorEngine()
+
+df = market.get_stock_data("RELIANCE.NS")
+
+df = indicator.calculate_indicators(df)
+
+print(df.tail())
